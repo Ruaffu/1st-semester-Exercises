97db30f65027e8a8262f51c1150f944d81d08f46
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2021-02-25 15:23:32 +0100

    Completed

--- Week 8/TaskThree/Datamatik.java
+++ Week 8/TaskThree/Datamatik.java
@@ -2,8 +2,8 @@ class Datamatik{
 	
 	public static void main(String[] args){
 		Teacher myTeacher = new Teacher("Tess", 47, true);
-		Student student1 = new Student("Christian", 35, false, "Team A");
-		Student student2 = new Student("Peter", 20, false, "Team A");
+		Student student1 = new Student("Christian", 35, false, 'A');
+		Student student2 = new Student("Peter", 20, false, 'A');
 
 
 		System.out.println("Teacher: " + myTeacher.name);
