023ef5e57ea293d22b91cb48949e932970bd4fe3
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2021-02-25 15:23:40 +0100

    update

--- Week 8/TaskFive/Datamatik.java
+++ Week 8/TaskFive/Datamatik.java
@@ -1,16 +1,16 @@
 class Datamatik{
 	public static void main(String[] args){
     	
-		Student student1 = new Student("Christian", 35, false, "l-dat-da 0221a");
-		Student student2 = new Student("Peter", 20, false, "l-dat-da 0221a");
-		Student student3 = new Student("Christian", 19, false, "l-dat-da 0221a");
-		Student student4 = new Student("Mads", 41, false, "l-dat-da 0221a");
-		Student student5 = new Student("Rabee", 48, false, "l-dat-da 0221a");
-		Student student6 = new Student("Angeliza", 25, true, "l-dat-da 0221a");
-		Student student7 = new Student("Louise", 23, true, "l-dat-da 0221a");
-		Student student8 = new Student("Victoria", 20, true, "l-dat-da 0221a");
-		Student student9 = new Student("Philippe", 25, false, "l-dat-da 0221a");
-		Student student10 = new Student("Oliver", 22, false, "l-dat-da 0221a");
+		Student student1 = new Student("Christian", 35, false, 'A');
+		Student student2 = new Student("Peter", 20, false, 'A');
+		Student student3 = new Student("Christian", 19, false, 'A');
+		Student student4 = new Student("Mads", 41, false, 'A');
+		Student student5 = new Student("Rabee", 48, false, 'A');
+		Student student6 = new Student("Angeliza", 25, true, 'A');
+		Student student7 = new Student("Louise", 23, true, 'A');
+		Student student8 = new Student("Victoria", 20, true, 'A');
+		Student student9 = new Student("Philippe", 25, false, 'A');
+		Student student10 = new Student("Oliver", 22, false, 'A');
 
 		
 		Student[] studentArray = {student1, student2, student3, student4, student5, student6, student7, student8, student9, student10};
